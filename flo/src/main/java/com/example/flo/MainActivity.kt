943bbcde389729e4d7f917a50b9cf81a26ac0634
se05503 <seyeongb18@gmail.com>
2025-09-28 14:28:37 +0900

    [feat] 액티비티간 데이터 전달하기 - registerForActivityResult

--- flo/src/main/java/com/example/flo/MainActivity.kt
+++ flo/src/main/java/com/example/flo/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.flo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.flo.databinding.ActivityMainBinding
@@ -12,6 +14,7 @@ import com.example.flo.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var getResultFromSongActivity: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +29,31 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        // main activity ↔ song activity
+        getResultFromSongActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if(result.resultCode == RESULT_OK) {
+                val songActivityAlbumTitle = result.data?.getStringExtra(INTENT_KEY)
+                Toast.makeText(this, "앨범 제목: $songActivityAlbumTitle", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        initListeners()
+    }
+
+    private fun initListeners() = with(binding) {
+        musicPlayerContainer.setOnClickListener {
+            val intent = Intent(this@MainActivity, SongDetailActivity::class.java).apply {
+                putExtra(TITLE, tvMusicTitle.text.toString())
+                putExtra(SINGER, tvMusicSinger.text.toString())
+            }
+            getResultFromSongActivity.launch(intent)
+        }
+    }
+    
+    companion object {
+        const val INTENT_KEY = "key"
+        const val TITLE = "title"
+        const val SINGER = "singer"
     }
 }
--- flo/src/main/java/com/example/flo/SongDetailActivity.kt
+++ flo/src/main/java/com/example/flo/SongDetailActivity.kt
@@ -1,20 +1,45 @@
 package com.example.flo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.flo.databinding.ActivitySongDetailBinding
 
 class SongDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySongDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_song_detail)
+        binding = ActivitySongDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initViews()
+        initListeners()
+    }
+
+    private fun initViews() = with(binding) {
+        val musicTitle = intent.getStringExtra(MainActivity.TITLE)
+        val musicSinger = intent.getStringExtra(MainActivity.SINGER)
+        ivDetailMusicTitle.text = musicTitle
+        ivDetailMusicSinger.text = musicSinger
+    }
+
+    private fun initListeners() = with(binding) {
+        ivDetailMusicCollapse.setOnClickListener {
+            val intent = Intent(this@SongDetailActivity, MainActivity::class.java).apply {
+                putExtra(MainActivity.INTENT_KEY, ivDetailMusicTitle.text.toString())
+            }
+            setResult(RESULT_OK, intent)
+            finish()
         }
     }
 }
