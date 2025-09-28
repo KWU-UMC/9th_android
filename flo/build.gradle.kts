2763bef19d3be431ad1c8e465b4f7c29ce205ec8
se05503 <seyeongb18@gmail.com>
2025-09-28 18:37:15 +0900

    [chore] 의존성 등 기타 설정

--- flo/build.gradle.kts
+++ flo/build.gradle.kts
@@ -9,16 +9,20 @@ plugins {
 
 android {
     namespace = "com.example.flo"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.flo"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    viewBinding {
+        enable = true
     }
 
     buildTypes {
@@ -44,6 +48,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
