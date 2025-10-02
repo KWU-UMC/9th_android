4e3b3b63fec93304ef1b19e3c086e409022b3af9
se05503 <seyeongb18@gmail.com>
2025-10-02 17:48:25 +0900

    [feat] 앨범 화면 TabLayout, Viewpager2 구현

---
+++ flo/src/main/java/com/example/flo/AlbumAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.flo
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class AlbumAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> AlbumListFragment()
+            1 -> AlbumDetailFragment()
+            2 -> AlbumVideoFragment()
+            else -> AlbumListFragment()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+}
--- flo/src/main/java/com/example/flo/AlbumFragment.kt
+++ flo/src/main/java/com/example/flo/AlbumFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.flo.databinding.FragmentAlbumBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment : Fragment(R.layout.fragment_album) {
 
@@ -22,6 +23,16 @@ class AlbumFragment : Fragment(R.layout.fragment_album) {
         tvAlbumSongTitle.text = albumData.title
         tvAlbumSongSinger.text = albumData.singer
         ivAlbumThumbnail.setImageResource(albumData.thumbnail)
+
+        albumViewPager2.adapter = AlbumAdapter(this@AlbumFragment)
+        TabLayoutMediator(tblAlbumIndicator, albumViewPager2) { tab, position ->
+            tab.text = when(position) {
+                0 -> "수록곡"
+                1 -> "상세정보"
+                2 -> "영상"
+                else -> ""
+            }
+        }.attach()
     }
 
 }
