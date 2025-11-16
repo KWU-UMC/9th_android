b02f475c9489e0fae31fa1b554967b9d3374decd
se05503 <seyeongb18@gmail.com>
2025-11-16 17:00:41 +0900

    chore: 패키지 분류에 따른 파일 수정

--- flo/src/main/java/com/example/flo/mission/presentation/home/HomeBannerAdapter.kt
+++ flo/src/main/java/com/example/flo/mission/presentation/home/HomeBannerAdapter.kt
@@ -2,9 +2,6 @@ package com.example.flo.mission.presentation.home
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.flo.home.HomeBannerOneFragment
-import com.example.flo.home.HomeBannerThreeFragment
-import com.example.flo.home.HomeBannerTwoFragment
 
 class HomeBannerAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
     override fun createFragment(position: Int): Fragment {
